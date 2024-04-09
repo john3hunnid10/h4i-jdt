b01ad55fc0830ccdf8b308c1447ab749cbd71b80
john3hunnid10 <johnadamsnyc@gmail.com>
2024-04-09 18:50:13 -0400

    navbar started

---
+++ code/Andrew/frontend/react-todo/vite-project/src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './navbar';
+function Navbar(){
+    return(
+        <nav className="navbar">
+            Todo.ai
+        </nav>
+    );
+}
+export default Navbar;
